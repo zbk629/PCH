4bfaa80ae0d3933b78fc9a5522b614b58559d4de
zhangchao <583902049@qq.com>
2016-10-09 11:00:52 +0800

    Revert "update"
    This reverts commit db63ca7fce3a47c4fde0cd715f49c36cbe182211.

--- src/main/java/com/cyparty/laihui/mapper/RoutePointMapper.java
+++ src/main/java/com/cyparty/laihui/mapper/RoutePointMapper.java
@@ -18,11 +18,11 @@ public class RoutePointMapper implements RowMapper<RoutePoint> {
         point.setRoute_id(resultSet.getInt("route_id"));
         point.setPoint_seq(resultSet.getInt("point_seq"));
         point.setPoint_name(resultSet.getString("point_name"));
-        /*point.setPoint_lat(resultSet.getString("point_lat"));
+        point.setPoint_lat(resultSet.getString("point_lat"));
         point.setPoint_lng(resultSet.getString("point_lng"));
         point.setPoint_uid(resultSet.getString("point_uid"));
         point.setPoint_city(resultSet.getString("point_city"));
-        point.setPoint_district(resultSet.getString("point_district"));*/
+        point.setPoint_district(resultSet.getString("point_district"));
         point.setPoint_create_time(resultSet.getString("point_create_time"));
 
 
