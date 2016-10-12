66def6a4fa0850a72003603b39aa5e7d103ab166
zhubangkui <1546232926>
2016-10-12 09:26:24 +0800

    /laihui/share

--- src/main/java/com/cyparty/laihui/controller/DepartureInfoController.java
+++ src/main/java/com/cyparty/laihui/controller/DepartureInfoController.java
@@ -2,7 +2,6 @@ package com.cyparty.laihui.controller;
 
 import com.cyparty.laihui.db.LaiHuiDB;
 import com.cyparty.laihui.domain.User;
-import com.cyparty.laihui.utilities.WXUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * Created by zhu on 2016/5/11.
@@ -19,6 +17,7 @@ import java.io.IOException;
 public class DepartureInfoController {
     @Autowired
     LaiHuiDB laiHuiDB;
+
     @RequestMapping("/laihui/car/list")
     public String car_departure(Model model, HttpServletRequest request) {
        /* //微信分享模块
@@ -32,18 +31,25 @@ public class DepartureInfoController {
         return "car_driver_departure_list";
     }
 
+    @RequestMapping("/laihui/share")
+    public String passenger_order_info(Model model, HttpServletRequest request) {
+
+        return "laihui_share";
+
+    }
+
     @RequestMapping("/laihui/car/detail")
-    public String car_departure_info(Model model, HttpServletRequest request,HttpServletResponse response) {
+    public String car_departure_info(Model model, HttpServletRequest request, HttpServletResponse response) {
         //微信分享模块
         String token = request.getParameter("token");
         String open = request.getParameter("open");
        /* String title = "来回拼车，你值得拥有！";
         WXUtils.wx_intro(request, model, title, "eat.gif");*/
-        if(open!=null&&!open.trim().equals("")){
-            String where =" where user_map_openid='"+open+"'";
-            if(laiHuiDB.getWxUser(where).size()>0){
-                User now_user=laiHuiDB.getWxUser(where).get(0);
-                request.getSession().setAttribute("user",now_user);
+        if (open != null && !open.trim().equals("")) {
+            String where = " where user_map_openid='" + open + "'";
+            if (laiHuiDB.getWxUser(where).size() > 0) {
+                User now_user = laiHuiDB.getWxUser(where).get(0);
+                request.getSession().setAttribute("user", now_user);
                 request.getSession().setAttribute("user_id", now_user.getUser_id());
             }
         }
@@ -53,14 +59,14 @@ public class DepartureInfoController {
             request.getSession().setAttribute("role", "1");
             request.getSession().setAttribute("own_id", id);
             model.asMap().clear();
-            return "redirect:/laihui/car/detail?id="+id;
+            return "redirect:/laihui/car/detail?id=" + id;
         } else {
             String now_id = request.getParameter("id");
             //|| request.getSession().getAttribute("role").equals("0")
-            if (request.getSession().getAttribute("role") == null ) {
+            if (request.getSession().getAttribute("role") == null) {
                 request.getSession().setAttribute("role", "0");
             } else {
-                if(request.getSession().getAttribute("own_id")!=null){
+                if (request.getSession().getAttribute("own_id") != null) {
                     if (!request.getSession().getAttribute("own_id").equals(now_id)) {
                         request.getSession().setAttribute("role", "0");
                     } else {
